fa1030771a26f8697a79cc7cdae67d230df620e9
AmritSamra25 <amritsamra2003@gmail.com>
2026-02-02 14:19:42 -0700

    Update FootballCanvas.tsx

--- src/canvas/FootballCanvas.tsx
+++ src/canvas/FootballCanvas.tsx
@@ -1,22 +1,19 @@
-import { Ellipse } from "react-konva";
-
 type FootballCanvasProps = {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 };
 
 export default function FootballCanvas({ x, y }: FootballCanvasProps) {
-    return (
-        <Ellipse
-            x={x}
-            y={y}
-            radiusX={6}
-            radiusY={3}
-            rotation={-25}
-            fill="brown"
-            stroke="white"
-            strokeWidth={0.8}
-        />
-    )
-
+  return (
+    <Ellipse
+      x={x}
+      y={y}
+      radiusX={6}
+      radiusY={3}
+      rotation={-25}
+      fill="#8b4513"
+      stroke="white"
+      strokeWidth={0.8}
+    />
+  );
 }
